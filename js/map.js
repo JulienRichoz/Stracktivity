ecef5c226cb7d3a330c725044cee288485c89b42
Julien Richoz <julienrichoz@outlook.com>
2019-02-14 14:29:58 +0100

    Move script from html to js

--- js/map.js
+++ js/map.js
@@ -1,3 +1,11 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('google-map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
 $.ajax({
     type: "GET",
     url: "../gpx/Afternoon_Ride.gpx",
