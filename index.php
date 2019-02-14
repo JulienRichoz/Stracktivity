97d2deb8fc586d4a87d5f7480e6b3860fb33104f
Julien Richoz <julienrichoz@outlook.com>
2019-02-14 13:52:24 +0100

    Basic structure of website

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	require_once("config/config.php")
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Stracktivity</title>
+	<meta name="viewport" content="initial-scale=1.0">
+	<meta charset="utf-8">
+	<link rel="icon" href="logo.png">
+   	<link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <!-- Insert library (bootstrap, jquery etc.) -->
+</head>
+
+<body>
+</body>
+</html>
