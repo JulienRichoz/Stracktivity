13d2a630de622260d8bf6310ed838812ba4bd955
Julien Richoz <julienrichoz@outlook.com>
2019-02-14 14:18:33 +0100

    Add libraries / scripts

--- index.php
+++ index.php
@@ -11,9 +11,27 @@
 	<link rel="icon" href="logo.png">
    	<link href="css/style.css" rel="stylesheet" type="text/css">
 
-    <!-- Insert library (bootstrap, jquery etc.) -->
+    <!-- jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- jquery-ui -->
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<!-- bootstrap -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 
 <body>
+    <div id="map">
+
+        <div id="google-map"></div>
+	</div>
+
+    <!-- d3js -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.js"></script>
+	<!-- Functions used for different operations -->
+	<!-- Map showing all the tracks -->
+	<script src="js/map.js"></script>
+	<!-- google maps api -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=<?= GOOGLE_MAPS_KEY; ?>&libraries=visualization&callback=initMap" async defer></script>
+</body>
 </body>
 </html>
