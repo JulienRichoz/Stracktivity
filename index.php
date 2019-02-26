648a57cb80493703309854c70d3d6546349d14bd
Julien Richoz <julienrichoz@outlook.com>
2019-02-26 20:54:01 +0100

    Add bone structure with activity elements

--- index.php
+++ index.php
@@ -4,21 +4,24 @@
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Stracktivity</title>
-	<meta name="viewport" content="initial-scale=1.0">
+	<title>Stracktivity</title>
+<meta name="viewport" content="initial-scale=1.0">
 	<meta charset="utf-8">
 	<link rel="icon" href="logo.png">
-   	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+	 crossorigin="anonymous">
 	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<!-- jquery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<!-- jquery-ui -->
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<!-- bootstrap -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+	 crossorigin="anonymous"></script>
 	<!-- chart js -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
 	<script src="js/env.js"></script>
@@ -26,18 +29,44 @@
 </head>
 
 <body>
-    <div id="map">
+	<div id="map">
 
-        <div id="google-map"></div>
+		<div id="google-map"></div>
+
+		<label class="filter" style="bottom: 250px;"><img src="img/mtb_icon.png" alt="Mountain Bike" class="img-thumbnail img-check check"
+			 value="1"><input type="checkbox" class="label_checkbox" autocomplete="off"></label>
+		<label class="filter" style="bottom: 190px;"><img src="img/hiking_icon.png" alt="Hiking" class="img-thumbnail img-check check"
+			 value="2"><input type="checkbox" class="label_checkbox" autocomplete="off"></label>
+		<label class="filter" style="bottom: 130px;"><img src="img/skitour_icon.png" alt="Ski Tour" class="img-thumbnail img-check check"
+			 value="3"><input type="checkbox" class="label_checkbox" autocomplete="off"></label>
+		<label class="filter" style="bottom: 70px;"><img src="img/other_icon.png" alt="Other" class="img-thumbnail img-check check"
+			 value="4"><input type="checkbox" class="label_checkbox" autocomplete="off"></label>
 	</div>
 
-    <!-- d3js -->
+	<div id="track"></div>
+
+	<div id="details">
+		<div id="data">
+			</div>
+
+			<h4>Profil du parcours</h4>
+		</div>
+
+		<div id="progress-bars">
+		
+		</div>
+	</div>
+
+
+	<!-- d3js -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.js"></script>
-	<!-- Functions used for different operations -->
+	<!-- Track class -->
+	<script src="js/track.js"></script>
 	<!-- Map showing all the tracks -->
 	<script src="js/map.js"></script>
 	<!-- google maps api -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=<?= GOOGLE_MAPS_KEY; ?>&libraries=visualization&callback=initMap" async defer></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=<?= GOOGLE_MAPS_KEY; ?>&libraries=visualization&callback=initMap"
+	 async defer></script>
 </body>
-</body>
+
 </html>
