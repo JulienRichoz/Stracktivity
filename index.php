cea9d7cf3b8e8b6fa5fde1962a5440c32de87065
Julien Richoz <julienrichoz@outlook.com>
2019-02-26 18:06:44 +0100

    Add bootstrap, chart, jquery src

--- index.php
+++ index.php
@@ -11,12 +11,18 @@
 	<link rel="icon" href="logo.png">
    	<link href="css/style.css" rel="stylesheet" type="text/css">
 
-    <!-- jquery -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<!-- jquery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<!-- jquery-ui -->
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<!-- bootstrap -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!-- chart js -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
+	<script src="js/env.js"></script>
+	<!--<script src="js/charts2.0.js"></script>-->
 </head>
 
 <body>
