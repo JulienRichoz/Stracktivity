3ab29166e97fa99942a62bc0e3f9eede2cb32acd
Julien Richoz <julienrichoz@outlook.com>
2019-03-07 14:55:23 +0100

    Add refersh location

--- fileManager/upload.js
+++ fileManager/upload.js
@@ -17,7 +17,6 @@ form.addEventListener('submit', e => {
         method: 'POST',
         body: formData
     }).then(response => {
-        console.log(response)
-        //location.reload();
+        location.reload();
     });
 });
