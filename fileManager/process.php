255442331583aa9bb38e8bf08cfcb7d49bf6d318
Julien Richoz <julienrichoz@outlook.com>
2019-03-07 14:54:29 +0100

    Remove useless Information

--- fileManager/process.php
+++ fileManager/process.php
@@ -12,7 +12,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $file_name = $_FILES['files']['name'][$i];
             $file_tmp = $_FILES['files']['tmp_name'][$i];
             $file_type = $_FILES['files']['type'][$i];
-            $file_size = $_FILES['files']['size'][$i];
             $file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$i])));
 
             $file = $path . $file_name;
